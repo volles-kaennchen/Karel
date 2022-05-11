f997f1ab565b20ba6e68b7ea2244033bb9e5ee37
Vanessa <105215748+VaniEF@users.noreply.github.com>
2022-05-11 20:17:12 +0200

    Update karel.cmd

--- karel.cmd
+++ karel.cmd
@@ -1,1 +1,6 @@
-java -version 
+cd Downloads
+if exist karel.jar.zip erase karel.jar.zip
+curl -o karel.jar https://raw.githubusercontent.com/fredoverflow/karel/release/karel.jar
+echo java -version > karel.cmd
+echo java -jar karel.jar >> karel.cmd
+karel.cmd
